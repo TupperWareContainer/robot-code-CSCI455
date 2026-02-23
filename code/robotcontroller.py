3df6da7924e55390833c402377b8ba38154de1bd
John <johnjamisonfoth@gmail.com>
2026-02-23 12:50:29 -0700

    implimented getters and setters for robotcontroller interface

--- code/robotcontroller.py
+++ code/robotcontroller.py
@@ -1,7 +1,15 @@
 from robot import Robot
 from enum import Enum
+'''
+RobotController.py
+Command Based Interface for controlling a Robot instance
+
+'''
+
+
 
 class RobotAction(Enum):
+    NONE = 0 
     HEAD_YES = 1
     HEAD_NO = 2
     ARM_RAISE = 3
@@ -15,17 +23,42 @@ class RobotState(Enum):
 
 class RobotController:
     __robotInstance : Robot
+    __scope : list[str]
+    __actionQueue : list[RobotAction]
+    __state : RobotState
     def __init__(self):
+        self.__actionQueue = list[RobotAction]
+        self.__state = RobotState.BOOT
         self.__robotInstance = Robot()
-        self.__rule_scope = (-1,-1)
+        self.__scope = list[str]
+
+   
+    def __StateMachine(self):
+        match self.__state:
+            case RobotState.BOOT:
+                pass
+            case RobotState.ACTION_EXEC:
+                pass
+            case _:
+                pass
 
 
-    def DoAction(action : RobotAction):
+
+    def AddAction(self, action : RobotAction):
+        self.__actionQueue.append(action) 
+
+    
+
+    def GetState(self) -> RobotState:
         pass
 
-    def GetState() -> RobotState:
-        pass
+    def GetCurrentAction(self) -> RobotAction: 
+        if (len(self.__actionQueue) <= 0):
+            return RobotAction.NONE
+        else: 
+            return self.__actionQueue[0]
 
-    def GetCurrentAction() -> RobotAction:
-        pass
 
+    
+    def GetScope(self) -> list[str]:
+        return self.__scope
