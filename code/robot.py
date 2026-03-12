835f3582798d5670ab86417a3d3186a386d1e5fb
John <johnjamisonfoth@gmail.com>
2026-03-12 15:16:44 -0600

    added time limit safety feature for robot

--- code/robot.py
+++ code/robot.py
@@ -14,6 +14,8 @@ class Robot:
     arm     : ArmController
     voice   : Voice
 
+    __MOTORCHANNELS = [3,4,5,0,1,6]
+
     def __init__(self):
         # Add the logic for tty1 vs tty0 here
         self.master_controller = maestro.Controller()
@@ -28,6 +30,17 @@ class Robot:
     def close(self):
         self.master_controller.close()
 
+    def StopAllChannels(self):
+        for channel in self.__MOTORCHANNELS:
+            if(self.master_controller.isMoving(channel)): 
+                self.master_controller.setSpeed(channel,self.master_controller.getPosition(channel))
+        self.turn_wheels(6000)
+        self.drive_wheels(6000)
+
+    def ResetServoPositions(self):
+        for channel in self.__MOTORCHANNELS:
+            if(channel != 0 and channel != 1):
+                self.master_controller.setTarget(6000)
 
     def pan_head(self, angle):
         self.head.pan(angle, 3)
--- code/robotcontroller.py
+++ code/robotcontroller.py
@@ -1,7 +1,9 @@
+import threading
+import time
 import robot_actions
 from robot import Robot
 from enum import Enum
-from collections import deque 
+from collections import deque  
 '''
 RobotController.py
 Command Based Interface for controlling a Robot instance
@@ -32,6 +34,11 @@ class RobotController:
     __state : RobotState
 
     __isPerformingAction : bool
+    __isSafetyTimerActive : bool
+
+    __cSafetyTime : float
+
+    __safety_thread : threading.Thread
 
     def __init__(self):
         self.__actionQueue = deque[RobotAction]()
@@ -39,6 +46,15 @@ class RobotController:
         self.__robotInstance = Robot()
         self.__scope = list[str]
         self.__isPerformingAction = False
+        self.__isSafetyTimerActive = False
+        self.__lastSafetyTime = -1
+        self.__maxSafetyTime = 0
+        self.__safeTimeSet = False
+
+        self.__safety_thread = threading.Thread(target=self.__SafetyTimer)
+
+        self.__safety_thread.start()
+
 
     def Update(self):
         if (len(self.__actionQueue) > 0) or self.__isPerformingAction: 
@@ -50,11 +66,33 @@ class RobotController:
         self.__StateMachine()
 
     
+    def __SafetyTimer(self):
+        if(self.__isSafetyTimerActive):
+            print("WARNING : ATTEMPTED CALL OF SAFETY TIMER WHILE TIMER IS ALREADY ACTIVE! Refusing to call method...")
+            return
+        self.__isSafetyTimerActive = True
+        while(True):
+            if(not self.__safeTimeSet):
+                continue
+            elif(time.time() - self.__lastSafetyTime >= self.__cSafetyTime):
+                self.__robotInstance.StopAllChannels()
+                self.__robotInstance.ResetServoPositions()
+
+                self.__safeTimeSet = False
+            time.sleep(1)
+
+                
+
+
+
+
+
     def __StateMachine(self):
         match self.__state:
             case RobotState.BOOT:
                 pass
             case RobotState.ACTION_EXEC:
+                
                 self.__PerformAction()
                 pass
             case _:
@@ -65,15 +103,32 @@ class RobotController:
 
         match actionID:
             case RobotAction.HEAD_YES:
+                self.__maxSafetyTime = 3
+
+                self.__lastSafetyTime = time.time()
+                self.__safeTimeSet = True
                 robot_actions.PerformHeadNod(self.__robotInstance)
                 pass
             case RobotAction.HEAD_NO:
+                self.__maxSafetyTime = 3
+
+                self.__lastSafetyTime = time.time()
+                self.__safeTimeSet = True
+
                 robot_actions.ShakeHead(self.__robotInstance)
                 pass
             case RobotAction.ARM_RAISE:
+                self.__maxSafetyTime = 4
+
+                self.__lastSafetyTime = time.time()
+                self.__safeTimeSet = True
                 robot_actions.RaiseArm(self.__robotInstance)
                 pass
             case RobotAction.DANCE_90:
+                self.__maxSafetyTime = 6
+
+                self.__lastSafetyTime = time.time()
+                self.__safeTimeSet = True
                 robot_actions.Dance90(self.__robotInstance)
                 pass
 
@@ -124,7 +179,13 @@ class RobotController:
         else: 
             return self.__actionQueue[0]
 
-
     
+    def Reset(self):
+        self.__actionQueue.clear()
+        self.__isPerformingAction = False
+        self.__state = RobotState.IDLE
+        self.__robotInstance.StopAllChannels()
+        self.__robotInstance.ResetServoPositions()
+
     def GetScope(self) -> list[str]:
         return self.__scope
