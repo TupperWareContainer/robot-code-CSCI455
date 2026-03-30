95fd4230e547d7c21d5a27b6a259e7af1a878631
rndmaccess <rndm.access.dev@gmail.com>
2026-03-30 17:09:28 -0600

    Force the body size to normal

--- code/LidarController.py
+++ code/LidarController.py
@@ -29,7 +29,7 @@ class LidarController:
         while not self.__stopScan:
             try:
                 # iter_scans is a blocking generator
-                for scan in self.__lidar.iter_scans(max_buf_meas=1500):
+                for scan in self.__lidar.iter_scans(scan_type='normal', max_buf_meas=1500):
                     for _, angle, distance in scan:
                         idx = min(359, int(floor(angle)))
                         self.__scan_data[idx] = distance
