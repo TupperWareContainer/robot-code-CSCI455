fac2d586677daecd24dfa9fe035a729532d716a1
rndmaccess <rndm.access.dev@gmail.com>
2026-03-31 16:28:50 -0600

    Removed the print distance debug message

--- code/LidarController.py
+++ code/LidarController.py
@@ -33,7 +33,6 @@ class LidarController:
                     for _, angle, distance in scan:
                         idx = min([359, int(floor(angle))])
                         self.__scan_data[idx] = distance
-                        print(distance)
 
                     time.sleep(0.1)  # Yields control to other threads
             except RPLidarException as e:
