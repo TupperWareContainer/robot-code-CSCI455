829d8b6bedac4cf5633567ee9c1f4fd6498f1091
rndmaccess <rndm.access.dev@gmail.com>
2026-03-31 15:59:56 -0600

    Added back in timeout

--- code/LidarController.py
+++ code/LidarController.py
@@ -9,7 +9,7 @@ class LidarController:
     __scan_thread : Thread
     __stopScan : bool
     def __init__(self, lidar_port : str, timeout : float, max_distance : float):
-        self.__lidar = RPLidar(None, lidar_port, baudrate=115200)
+        self.__lidar = RPLidar(None, lidar_port, timeout=timeout, baudrate=115200)
         self.__max_distance = max_distance
         self.__stopScan = False
         self.__scan_thread = Thread(target = self.StartScan)
