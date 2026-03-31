3ad98305f468c84b9b40e4bdbbd9f089bf4e2a8c
rndmaccess <rndm.access.dev@gmail.com>
2026-03-31 12:12:37 -0600

    Its once again tied to the instance

--- code/LidarController.py
+++ code/LidarController.py
@@ -8,13 +8,13 @@ class LidarController:
     __max_distance : float
     __scan_thread : Thread
     __stopScan : bool
-    __scan_data = [0.0] * 360
     def __init__(self, lidar_port : str, timeout : float, max_distance : float):
         self.__lidar = RPLidar(None, lidar_port, baudrate=115200, timeout=timeout)
         self.__max_distance = max_distance
         self.__stopScan = False
         self.__scan_thread = Thread(target = self.StartScan)
         self.__scan_thread.start()
+        self.__scan_data = [0.0] * 360
 
     def StartScan(self):
         try:
