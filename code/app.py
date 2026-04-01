e4c424d4a87830954eda32156fce588947859f62
rndmaccess <rndm.access.dev@gmail.com>
2026-04-01 11:20:15 -0600

    Tried to CanMove

--- code/app.py
+++ code/app.py
@@ -87,7 +87,7 @@ def drive():
         # 6000 is center/neutral, above = forward, below = backward
         direction = "forward" if throttle > 6000 else "backward"
 
-        if not controller.CanMove(direction):
+        if controller.CanMove(direction):
             tempstop()
             return jsonify({"response": "Obstacle"}), 200
 
