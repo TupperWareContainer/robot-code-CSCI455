4bbf52e6038d9a722b32148a623308b3d09cbb68
rndmaccess <rndm.access.dev@gmail.com>
2026-04-08 18:59:21 -0600

    Lets try testing again

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -69,7 +69,7 @@ class LidarController:
         with self.__data_lock:
             distance_mm = self.__scan_data[angle]
             print(distance_mm)
-            print("Angle: " + str(angle)  + " distance: " + str(distance_mm))
+            print(f"id(scan_data): {id(self.__scan_data)} | wrote {distance_mm} to {angle}")
         #if distance_mm <= 0:
         #    return -1
         return distance_mm
