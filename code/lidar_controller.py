458fe373daa64c923929bc704e8f27b2a0a78c3e
rndmaccess <rndm.access.dev@gmail.com>
2026-04-13 10:33:45 -0600

    It was a different typo

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -35,7 +35,7 @@ class LidarController:
             print("scanning") 
             try:
                 # iter_scans is a blocking generator
-                for (new_scan, quality, angle, distance) in self.__lidar.iter_measuments():
+                for (new_scan, quality, angle, distance) in self.__lidar.iter_measurments():
                     idx = min([359, floor(angle)])
                     self._scan_data[idx] = distance
 
