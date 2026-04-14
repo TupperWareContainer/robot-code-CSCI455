936ea2312aa78d41782d695c7fbd135437412d53
rndmaccess <rndm.access.dev@gmail.com>
2026-04-14 11:44:49 -0600

    Added clear input to the startup in the scan

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -28,6 +28,7 @@ class LidarController:
             self.__lidar.stop() 
             self.__lidar.disconnect()
             self.__lidar.connect()
+            self.__lidar.clear_input()
         except Exception as e:
             print(f"Unexpected Error: {e}")
         started = False
