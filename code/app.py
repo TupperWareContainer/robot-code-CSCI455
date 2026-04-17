2b9fe2db5efa1ac1a201e8b83bbc727a30807809
rndmaccess <rndm.access.dev@gmail.com>
2026-04-17 14:06:40 -0600

    Maybe app.run is a blocking call

--- code/app.py
+++ code/app.py
@@ -186,11 +186,12 @@ def main():
     thread.start()
 
     robot_controller.stop_drive()
-    app.config["SERVER_NAME"] = server_name
-    app.run(host=server_name, port=5002, debug=True, use_reloader=False)
 
     wall_follow_thread = Thread(target=robot_controller.WallFollowTick)
     wall_follow_thread.start()
+
+    app.config["SERVER_NAME"] = server_name
+    app.run(host=server_name, port=5002, debug=True, use_reloader=False)
 
 
 def exit_handler():
