b2e7fe50ca88b7205c531fa818383deee2824e0a
rndmaccess <rndm.access.dev@gmail.com>
2026-04-17 14:10:43 -0600

    Removed the debug prints

--- code/robot_controller.py
+++ code/robot_controller.py
@@ -307,11 +307,8 @@ class RobotController:
         self.__robotInstance.drive_wheels(6000)
 
     def WallFollowTick(self):
-        print("WallFollowTick")
-
         try:
             while True:
-                print("Running Wall Follow Tick...")
                 self.__state = RobotState.WALL_FOLLOW
 
                 if self.IsFrontBlocked():  # Case 1
