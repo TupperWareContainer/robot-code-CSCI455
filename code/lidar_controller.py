cf757c265df7cd0883f1fcf822a9fa521cd53c44
rndmaccess <rndm.access.dev@gmail.com>
2026-04-20 11:01:15 -0600

    Lets try a splice approach instead

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -58,8 +58,8 @@ class LidarController:
                     self._buffered_scan_data[idx] = distance # Write the distances to the buffer
 
                     if time.time() - self._last_update_time >= self._swap_interval:
-                        self._scan_data = self._buffered_scan_data # Move the contents of the buffer to scan data
-                        self._buffered_scan_data = [0] * 360  # Reset the buffer
+                        self._scan_data[:] = self._buffered_scan_data # Move the contents of the buffer to scan data
+                        self._buffered_scan_data[:] = [0] * 360  # Reset the buffer
                         self._last_update_time = time.time()
             except RPLidarException as e:
                 # This is where 'line length mismatch' is caught
