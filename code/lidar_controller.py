777529b2c2b5721b9af3408b25abb2c042f2211a
rndmaccess <rndm.access.dev@gmail.com>
2026-04-23 17:21:58 -0600

    Try putting the distance back to 0.0

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -43,7 +43,7 @@ class LidarController:
                         continue
 
                     if distance == 0.0:
-                        continue
+                        distance = 5000
 
                     idx = min([359, floor(angle)])
                     self._buffered_scan_data[idx] = distance # Write the distances to the buffer
