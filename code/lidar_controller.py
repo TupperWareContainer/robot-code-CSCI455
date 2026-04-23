77e281b296f0b01721695b6660f24b7a2c7e8672
rndmaccess <rndm.access.dev@gmail.com>
2026-04-23 17:42:46 -0600

    Swapped back the right and left turns and added a skip for 0.0 angles.

--- code/lidar_controller.py
+++ code/lidar_controller.py
@@ -41,7 +41,7 @@ class LidarController:
                         continue
 
                     if distance == 0.0:
-                        distance = 5000
+                        continue
 
                     idx = min([359, floor(angle)])
                     self._buffered_scan_data[idx] = distance # Write the distances to the buffer
