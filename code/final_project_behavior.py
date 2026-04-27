df84e9085698d28bbba1a201c43b91b96810287c
robot17@rasberrypy.com <robot17@rasberrypy.com>
2026-04-27 14:26:09 -0600

    got outline for final project behavior done

--- code/final_project_behavior.py
+++ code/final_project_behavior.py
@@ -1,5 +1,5 @@
 from robot_controller import RobotController
-
+import time
 
 def StartFinalProjectBehavior(controllerInstance : RobotController):
     while(not controllerInstance.IsFrontBlocked()):
@@ -13,12 +13,38 @@ def StartFinalProjectBehavior(controllerInstance : RobotController):
     # set destination depending on phrase
     pass
 
+def FinalProjectInitialization(controllerInstance: RobotController):
+    
+    ## turn 180 degrees
+    controllerInstance.steer_right()
+    time.sleep(1.4)
+    controllerInstance.stop_steer()
+   
+    # TODO: find a way to modify this value
+    destination = "Lab"
+    
+    startnextstage = False
 
-
+    while(startnextstage == False):
+        controllerInstance.drive(4500)
+        if(controllerInstance.IsFrontBlocked()):
+            controllerInstance.stop_drive()
+            time.sleep(4)
+            startnextstage = controllerInstance.IsFrontBlocked()
+            continue  
+    
+    controllerInstance.stop_drive()
+     
+    if(destination == "Lab"):
+        PathToLab(controllerInstance)
+    elif (destination == "Bathroom"):
+        PathToBathroom(controllerInstance)
+    
+    pass
 
 
 def PathToBathroom(controllerInstance : RobotController):
-      
+       
 
     pass
 
