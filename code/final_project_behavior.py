42984f86ef254d6327abd9c9da2b7c0eaf9218ed
rndmaccess <rndm.access.dev@gmail.com>
2026-04-28 19:01:22 -0600

    Exended the stop distance

--- code/final_project_behavior.py
+++ code/final_project_behavior.py
@@ -58,7 +58,7 @@ def FinalProjectInitialization(controllerInstance: RobotController):
     startnextstage = False
 
     while(not startnextstage):
-        is_blocked, is_init = controllerInstance.IsFrontBlocked(stop_dist=600)
+        is_blocked, is_init = controllerInstance.IsFrontBlocked(stop_dist=800)
 
         if (is_init):
             controllerInstance.drive(5000)
